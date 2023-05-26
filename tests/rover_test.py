1203d57b07bc22494f29db0914eaaada64109b4f
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-05-26 15:26:21 +0200

    clean up test

--- tests/rover_test.py
+++ tests/rover_test.py
@@ -2,13 +2,6 @@ import pytest
 from lib.rover import Rover
 from lib.rover import RoverState as RS
 
-def test_rover():
-    rover = Rover(0,0,"N")
-
-import pytest
-from lib.rover import Rover
-
-
 testcases = [
     (RS(0,0,"N"), "f", RS(0,1,"N")),
     (RS(0,1,"N"), "b", RS(0,0,"N")),
@@ -22,7 +15,7 @@ testcases = [
     (RS(3,3,"N"), "rr", RS(3,3,"S")),
              ]
 @pytest.mark.parametrize("init,command,expected", testcases)
-def test_rover(init, command, expected):
+def test_rover_move(init, command, expected):
     rover = Rover(init)
     rover.execute(command)
     assert rover.position() == expected
