4140e86fd74c29cf5cd1bd288c6806bf655c94de
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-05-26 15:07:59 +0200

    refactor

--- lib/rover.py
+++ lib/rover.py
@@ -1,18 +1,19 @@
 from dataclasses import dataclass
 class Rover:
     def __init__(self, rover_state):
+        self.state = rover_state
         self.x = rover_state.x
         self.y = rover_state.y
 
     def execute(self, command):
         if "f" == command:
-            self.y += 1
+            self.state.y += 1
         else:
-            self.y -= 1
+            self.state.y -= 1
         pass
 
     def position(self):
-        return RoverState(self.x, self.y, "N")
+        return self.state
     
 @dataclass
 class RoverState:
