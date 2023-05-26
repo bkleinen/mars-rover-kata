051ded420d62343908cc1b8c1c3afe3cbde4b4fc
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-05-26 14:56:10 +0200

    execute

--- lib/rover.py
+++ lib/rover.py
@@ -1,3 +1,9 @@
 class Rover:
-    def __init__(self,x,y,direction):
+    def __init__(self, x, y, direction):
         pass
+
+    def execute(self, command):
+        pass
+
+    def position(self):
+        return 0,1
--- tests/rover_test.py
+++ tests/rover_test.py
@@ -3,4 +3,14 @@ from lib.rover import Rover
 
 def test_rover():
     rover = Rover(0,0,"N")
+
+import pytest
+from lib.rover import Rover
+
+def test_rover_forward():
+    rover = Rover(0,0,"N")
+    rover.execute('f')
+    assert rover.position() == (0,1)
+
+
     
