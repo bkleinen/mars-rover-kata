db4c256eaf794d4cee67bcd96860fc0b533f0c3a
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-05-27 21:57:42 +0200

    add poles

--- lib/planet.py
+++ lib/planet.py
@@ -2,4 +2,9 @@ from lib.rover import World
 
 
 class Planet(World):
-    pass
+    def next(self, rover_state, command):
+        if not self.on_pole():
+            return super().next(rover_state, command)
+    
+    def on_pole(self):
+        return False
