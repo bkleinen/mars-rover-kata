9bfc7ad80ca9db65ae7f4d807aa102ae1b292279
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-05-27 22:06:49 +0200

    first Pole expedition successful

--- lib/planet.py
+++ lib/planet.py
@@ -5,11 +5,12 @@ from dataclasses import replace
 
 class Planet(World):
     def next(self, rover_state, command):
-        if not self.on_pole(rover_state):
-            return super().next(rover_state, command)
-        new_x = self.width - rover_state.pos.x
-        new_y = self.height-2
-        return replace(rover_state, pos=P(new_x, new_y), direction="S")
+        if self.on_north_pole(rover_state):
+            new_x = self.width - rover_state.pos.x
+            new_y = self.height-2
+            return replace(rover_state, pos=P(new_x, new_y), direction="S")
     
-    def on_pole(self, rover_state):
+        return super().next(rover_state, command)
+    
+    def on_north_pole(self, rover_state):
         return rover_state.pos.y == self.height-1
