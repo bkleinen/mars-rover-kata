93a5ec3df48cafc77dbad00079e59076b588ccc4
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-05-28 15:01:35 +0200

    refactor

--- lib/planet.py
+++ lib/planet.py
@@ -9,8 +9,7 @@ class Planet(World):
             raise AttributeError('width must be multiple of 4')
         super().__post_init__()
 
-    def next(self, rover_state, command):
-        
+    def next(self, rover_state, command): 
         if self.not_on_pole(rover_state):
             return super().next(rover_state, command)
         
@@ -18,12 +17,12 @@ class Planet(World):
         if self.on_north_pole(rover_state):
             new_y = self.height-2
             new_direction = 'S'
-            return replace(rover_state, pos=P(new_x, new_y), direction="S")
-        elif self.on_south_pole(rover_state):
+        else:
             new_y = 1
             new_direction = 'N'
-            return replace(rover_state, pos=P(new_x, new_y), direction="N")
-    
+         
+        return replace(rover_state, pos=P(new_x, new_y), direction=new_direction)
+
     def not_on_pole(self, rover_state):
         return not self.on_north_pole(rover_state) and not self.on_south_pole(rover_state)
     
