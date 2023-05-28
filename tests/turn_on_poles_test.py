47d65918189149a3c6eb1d98aaa3396c66dc1452
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-05-28 15:28:01 +0200

    red: encountered bug by accident

--- tests/turn_on_poles_test.py
+++ tests/turn_on_poles_test.py
@@ -17,7 +17,12 @@ testcases_planet = [(RS(0,2,"N"), "ff", RS(2,2,"S")),
                     (RS(0,3,"N"), "rf", RS(1,2,"S")),
                     (RS(0,3,"N"), "lf", RS(3,2,"S")),
                     (RS(1,3,"N"), "llf", RS(1,2,"S")),
-             ]
+
+                    (RS(1,3,"N"), "rrf",  RS(1,2,"S")),                           
+                    (RS(1,3,"S"), "f",  RS(1,2,"S")),
+                    (RS(1,3,"S"), "ff", RS(1,1,"S")), # this is on north pole and fails
+                  
+]
       
 @pytest.mark.parametrize("init,command,expected", testcases_planet)
 def test_rover_turn_on_pole_4_4(init, command, expected):
@@ -33,7 +38,8 @@ testcases_planet_8 = [
     (RS(0,7,"N"), "rf", RS(2,6,"S")),
     (RS(0,7,"N"), "rrf", RS(0,6,"S")),
     (RS(0,7,"N"), "lf", RS(6,6,"S")),
-    (RS(0,6,"N"), "frrf", RS(0,6,"S"))
+    (RS(0,6,"N"), "frrf", RS(0,6,"S")),
+
 ]
 @pytest.mark.parametrize("init,command,expected", testcases_planet_8)
 def test_rover_turn_on_pole_8_8(init, command, expected):
