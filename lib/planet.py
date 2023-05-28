e5d5d07fcf1fffddc5a8899e098bf15486206fcc
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-05-28 22:15:25 +0200

    whole pole...

--- lib/planet.py
+++ lib/planet.py
@@ -14,14 +14,14 @@ class Planet(World):
    #         return super().next_position(rover_state, command)
    #     return self.next_on_pole(rover_state)
 
-    def next(self, rover_state, command): 
-        if self.not_on_pole(rover_state):
-            return super().next(rover_state, command)
+    def next_state(self, rover_state, command): 
+        if self.not_on_pole(rover_state.pos):
+            return super().next_state(rover_state, command)
         
         return self.next_on_pole(rover_state)
 
     def next_on_pole(self, rover_state): 
-        if self.on_north_pole(rover_state):
+        if self.on_north_pole(rover_state.pos):
             turns = {'E': 1, 'N' : 2, 'W': 3, 'S': 4}
             new_y = self.height-2
             new_direction = 'S'
@@ -33,15 +33,18 @@ class Planet(World):
         new_x = self.turn(rover_state.pos.x,turns[rover_state.direction])   
         return replace(rover_state, pos=P(new_x, new_y), direction=new_direction)
 
-    def not_on_pole(self, rover_state):
-        return not self.on_north_pole(rover_state) and not self.on_south_pole(rover_state)
+    def not_on_pole(self, pos):
+        return not self.on_north_pole(pos) and not self.on_south_pole(pos)
     
-    def on_north_pole(self, rover_state):
-        return rover_state.pos.y == self.height-1
+    def on_north_pole(self, pos):
+        return pos.y == self.height-1
 
-    def on_south_pole(self, rover_state):
-        return rover_state.pos.y == 0
+    def on_south_pole(self, pos):
+        return pos.y == 0
     
     def turn(self, x, quarters):
         quarter = self.width/4
         return int((x + quarters * quarter) % self.width)
+    
+    def add_obstacle(self, x, y):
+        self.set(x,y,'o')   
--- tests/turn_on_poles_obstacles_test.py
+++ tests/turn_on_poles_obstacles_test.py
@@ -1,55 +1,83 @@
 import pytest
 from lib.rover import Rover
 from lib.planet import Planet
-from lib.base import RS
+from lib.base import RS, ObstacleEncountered
+
 
 planet_string = """
 ....
 ...o
-....
+.o..
 ....
 """
 
+
 testcases_planet = [
-    (RS(0,2,"N"), "ff", RS(2,2,"S")),
-  # (RS(1,2,"N"), "ff", RS(3,2,"S")),
-   # (RS(2,2,"N"), "ff", RS(0,3,"S")), # stops in front of obstacle
-  # (RS(3,2,"N"), "ff", RS(1,2,"S")),
-
-  # (RS(0,2,"N"), "frf", RS(1,2,"S")),
-  # (RS(1,2,"N"), "frf", RS(2,2,"S")),
-  # (RS(2,2,"N"), "frf", RS(3,2,"S")),
-  # (RS(3,2,"N"), "frf", RS(0,2,"S")),
-
-  # (RS(0,2,"N"), "flf", RS(3,2,"S")),
-  # (RS(1,2,"N"), "flf", RS(0,2,"S")),
-  # (RS(2,2,"N"), "flf", RS(1,2,"S")),
-  # (RS(3,2,"N"), "flf", RS(2,2,"S")),
-
-  #
-  # (RS(0,1,"S"), "ff", RS(2,1,"N")),
-  # (RS(1,1,"S"), "ff", RS(3,1,"N")),
-  # (RS(2,1,"S"), "ff", RS(0,1,"N")),
-  # (RS(3,1,"S"), "ff", RS(1,1,"N")),
-
-  # (RS(0,1,"S"), "frf", RS(3,1,"N")),
-  # (RS(1,1,"S"), "frf", RS(0,1,"N")),
-  # (RS(2,1,"S"), "frf", RS(1,1,"N")),
-  # (RS(3,1,"S"), "frf", RS(2,1,"N")),
-
-    (RS(0,1,"S"), "flf", RS(1,1,"N")),
-    (RS(1,1,"S"), "flf", RS(2,1,"N")),
-    (RS(2,1,"S"), "flf", RS(3,1,"N")),
-    (RS(3,1,"S"), "flf", RS(0,1,"N")),
-                    
+    (RS(1,2,"N"), "ff", RS(1,3,"N")), # stops in front of obstacle
+    (RS(3,1,"S"), "ff", RS(3,0,"S")),
+    (RS(2,1,"S"), "frf", RS(2,0,"W")),
+    (RS(0,1,"S"), "flf", RS(0,0,"E")),             
 ]
-      
+
 @pytest.mark.parametrize("init,command,expected", testcases_planet)
-def test_rover_turn_on_pole_4_4(init, command, expected):
+def test_rover_turn_on_pole_4_4_obstacles_invalid(init, command, expected):
     planet = Planet.from_str(planet_string)
-    assert str(type(planet)) == "<class 'lib.planet.Planet'>"
     rover = Rover(init, planet)
-    
+    with pytest.raises(ObstacleEncountered):
+        rover.execute(command)
+    assert rover.position() == expected
+
+planet_string = """
+oooo
+....
+....
+..o.
+"""
+
+# its possible to build a semi-permeable pole now:
+
+testcases_planet = [
+    (RS(3,1,"S"), "ff", RS(3,0,"S")),
+    (RS(0,1,"S"), "flf", RS(0,0,"E")),             
+]
+
+@pytest.mark.parametrize("init,command,expected", testcases_planet)
+def test_rover_turn_on_pole_4_4_obstacles_invalid(init, command, expected):
+    planet = Planet.from_str(planet_string)
+    rover = Rover(init, planet)
     rover.execute(command)
     assert rover.position() == expected
+
+
+testcases_planet = [
+    (RS(1,2,"N"), "f", RS(1,2,"N")), # stops in front of obstacle
+    (RS(2,1,"S"), "frf", RS(2,1,"S")),
+   ]
+
+@pytest.mark.parametrize("init,command,expected", testcases_planet)
+def test_rover_turn_on_pole_4_4_obstacles_invalid(init, command, expected):
+    planet = Planet.from_str(planet_string)
+    rover = Rover(init, planet)
+    with pytest.raises(ObstacleEncountered):
+        rover.execute(command)
+    assert rover.position() == expected
+
+
+# thus, the planet should look like this if an 
+# obstacle is set to the pole:
+planet_string = """
+oooo
+....
+....
+oooo
+"""
+@pytest.mark.xfail
+def test_obstacle_covers_pole():
+    planet = Planet(4,4)
+    planet.add_obstacle(0,0)
+    planet.add_obstacle(3,3)
+    assert str(planet) == planet_string
+
+
+
 
