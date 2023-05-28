b0e482de58519966944e4bc2a4e0692829609e8b
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-05-28 14:51:36 +0200

    first turn test

--- tests/turn_on_poles_test.py
+++ tests/turn_on_poles_test.py
@@ -12,10 +12,11 @@ def test_world_width_must_be_multiple_of_four():
 def test_turn_on_north_pole():
     pass
 
-testcases_planet = [(RS(0,2,"N"), "ff", RS(2,2,"S"))
+testcases_planet = [(RS(0,2,"N"), "ff", RS(2,2,"S")),
+                    (RS(0,3,"N"), "f", RS(2,2,"S"))
              ]
 
-new_tests = [(RS(0,2,"N"), "ff", RS(2,2,"S"))]
+new_tests = [(RS(0,3,"N"), "rf", RS(1,2,"S"))]
 
 new_tests = [pytest.param(*t,marks=pytest.mark.xfail) for t in new_tests]
 
@@ -23,7 +24,7 @@ testcases_planet.extend(new_tests)
  # pytest.param(
    #     RS(1,0,"S"), "f", RS(11,1,"N"),
    #     marks=pytest.mark.xfail), # passing over North Pole
-@pytest.mark.skip
+
 @pytest.mark.parametrize("init,command,expected", testcases_planet)
 def test_rover_turn_on_pole_4_4(init, command, expected):
     rover = Rover(init, Planet(4,4))
@@ -33,18 +34,21 @@ def test_rover_turn_on_pole_4_4(init, command, expected):
 
                       
 testcases_planet_8 = [
+    (RS(0,7,"N"), "f", RS(4,6,"S")),
+    (RS(0,6,"N"), "ff", RS(4,6,"S"))
             
              ]
-new_tests_8 = [(RS(0,7,"N"), "f", RS(4,6,"S")),(RS(0,6,"N"), "ff", RS(4,6,"S"))]
+new_tests_8 = [
+               ]
 
-new_tests_8 = [pytest.param(*t,marks=pytest.mark.xfail) for t in new_tests]
+new_tests_8 = [pytest.param(*t,marks=pytest.mark.xfail) for t in new_tests_8]
 
 testcases_planet_8.extend(new_tests_8)       
  # pytest.param(
    #     RS(1,0,"S"), "f", RS(11,1,"N"),
    #     marks=pytest.mark.xfail), # passing over North Pole
-@pytest.mark.skip
-@pytest.mark.parametrize("init,command,expected", testcases_planet)
+
+@pytest.mark.parametrize("init,command,expected", testcases_planet_8)
 def test_rover_turn_on_pole_8_8(init, command, expected):
     rover = Rover(init, Planet(8,8))
     rover.execute(command)
