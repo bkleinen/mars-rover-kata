ddb12ac2ed06c3f701691a86fda12bceec3492eb
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-05-30 19:19:31 +0200

    baby steps...

--- mars_rover/rover_state.py
+++ mars_rover/rover_state.py
@@ -49,39 +49,6 @@ class Orientation(Enum):
             }
     def quarter_turns_for_on(self, command, pole):
         return self.pole_quarter_turns()[pole][command][self]
-@dataclass(frozen=True)
-class Direction:
-    direction_str: str
-    
-    def delta(self, command):
-        deltas = {
-            'f' :  {'N' : P(0,1), 'S' : P(0,-1), 'W' : P(-1, 0), 'E': P(1, 0)},
-            'b' :  {'N' : P(0,-1), 'S' : P(0,1), 'W' : P(1, 0), 'E': P(-1, 0)}
-        }
-        delta = deltas[command][self.direction_str]
-        return delta
-
-
-    directions = ["N", "E", "S", "W"]
-    turns = {'r' : 1, 'l': -1}
-
-    def turn(self, command):
-        i = self.directions.index(self.direction_str)
-        delta_i = self.turns[command]
-        new_index = (i + delta_i) % len(self.directions)
-        new_direction = self.directions[new_index]
-        return new_direction
-        return Direction(new_direction)
-    
-    pole_quarter_turns = { 'N' : {
-            'f': {'E': 1, 'N' : 2, 'W': 3, 'S': 0},
-            'b': {'E': 3, 'N' : 0, 'W': 1, 'S': 2}},
-              'S' : {
-            'f': {'E': 1, 'N' : 0, 'W': 3, 'S': 2},
-            'b': {'E': 3, 'N' : 2, 'W': 1, 'S': 0}}
-            }
-    def quarter_turns_for_on(self, command, pole):
-        return self.pole_quarter_turns[pole][command][self.direction_str]
 
 
 @dataclass(frozen=False)
